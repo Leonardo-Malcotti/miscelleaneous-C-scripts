c514c8206dcc68f55f673b9e85589285dc0f2222
Leonardo-Malcotti <57906466+Leonardo-Malcotti@users.noreply.github.com>
2021-02-25 18:44:35 +0100

    creato uno script di installazione
    Da ricordare che è molto basilare, utilizza curl per salvare il codice in file temporanei e installa il tutto con advinstall dopo averlo compilato con gcc, non effettua alcun controllo.

---
+++ install.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+sudo
+curl -fsSL https://raw.githubusercontent.com/Leonardo-Malcotti/miscelleaneous-C-scripts/master/advinstall.c >> advinstall.c
+curl -fsSL https://raw.githubusercontent.com/Leonardo-Malcotti/miscelleaneous-C-scripts/master/advrm.c >> advrm.c 
+curl -fsSL https://raw.githubusercontent.com/Leonardo-Malcotti/miscelleaneous-C-scripts/master/nprog.c >> nprog.c 
+curl -fsSL https://raw.githubusercontent.com/Leonardo-Malcotti/miscelleaneous-C-scripts/master/utils.h >> utils.c 
+
+gcc advinstall.c -o advinstall
+./advinstall advrm.c 
+./advinstall nprog.c 
+./advinstall -self 
+
+echo "per utilizzare gli script aggiungi /usr/local/bin al tuo path di sistema"
+
+rm advinstall.c 
+rm advrm.c 
+rm nprog.c 
+rm utils.c 
+rm advinstall
+
+exit 0
