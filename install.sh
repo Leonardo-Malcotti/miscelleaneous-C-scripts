293f1e1c50269c29c7f0337b3060d7e5a76f07c4
Leonardo-Malcotti <57906466+Leonardo-Malcotti@users.noreply.github.com>
2021-02-26 09:48:44 +0100

    Aggiunti controlli all'installazione

--- install.sh
+++ install.sh
@@ -1,10 +1,27 @@
 #!/bin/bash
 
+#check OS
+OS="$(uname)"
+if [[ "$OS" == "Darwin" ]]; then
+	abort "Only MacOS (maybe linux) is supported."
+fi
+
+#check for curl and gcc
+if ! command -v curl >/dev/null; then
+    abort "You must have cURL installed to continue."
+fi
+
+if ! command -v gcc >/dev/null; then
+    abort "You must have a C compiler (gcc) installed to continue."
+fi
+
+#download files
 curl -fsSL https://raw.githubusercontent.com/Leonardo-Malcotti/miscelleaneous-C-scripts/master/advinstall.c >> advinstall.c
 curl -fsSL https://raw.githubusercontent.com/Leonardo-Malcotti/miscelleaneous-C-scripts/master/advrm.c >> advrm.c 
 curl -fsSL https://raw.githubusercontent.com/Leonardo-Malcotti/miscelleaneous-C-scripts/master/nprog.c >> nprog.c 
 curl -fsSL https://raw.githubusercontent.com/Leonardo-Malcotti/miscelleaneous-C-scripts/master/utils.h >> utils.h 
 
+#compile and install
 gcc advinstall.c -o advinstall
 ./advinstall advrm.c 
 ./advinstall nprog.c 
@@ -12,6 +29,7 @@ gcc advinstall.c -o advinstall
 
 echo "per utilizzare gli script aggiungi /usr/local/bin al tuo path di sistema"
 
+#clean
 rm advinstall.c 
 rm advrm.c 
 rm nprog.c 
