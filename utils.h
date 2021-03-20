18dec6a6ee01d1a575ef3f1dcb8d89cff503e88a
Leonardo-Malcotti <Leo.Malcotti@gmail,com>
2021-03-20 12:29:37 +0100

    aggiunta funzione strToUpper

--- utils.h
+++ utils.h
@@ -15,5 +15,18 @@ enum boolean{false,true};
 #define startWith(str,chr) (str[0]==chr)
 #define isFlag(str) startWith(str,'-')
 #define eql(str1,str2) (strcmp(str1,str2)==0)
+#define isLowerCase(chr) (chr>='a' && chr<='z')
+#define ToUp(chr) (chr - ('a'-'A'))
+#define ToLow(chr) (chr + ('a'-'A'))
+
+
+string strToUpper(string s){
+	string ret = newstring(strlen(s));
+	int i;
+	for(i=0;i<strlen(s);i++){
+		ret[i] = (isLowerCase(s[i])) ? ret[i] = ToUp(s[i]) : s[i];
+	}
+	return ret;
+}
 
 #endif
